f98645347553244f35782b6db492eb92208074f1
KeelyC0d3s <keelylacy@gmail.com>
2020-03-04 21:54:14 +0000

    squareroot homework

---
+++ squareroot.py
@@ -0,0 +1,14 @@
+# Keely's homework again.
+# Please enter a positive number: 14.5
+# The square root of 14.5 is approx. 3.8.
+# Now, time to figure out how to get the square root of 14.5
+
+import math
+
+math.sqrt(14.5)
+
+print( "math.sqrt(14.5)", math.sqrt(14.5))
+
+
+
+
